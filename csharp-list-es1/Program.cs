6d903f97be9c82347f10dd5c34a4006d2ce1bb5c
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 19:09:11 +0100

    first commit

---
+++ csharp-list-es1/Program.cs
@@ -0,0 +1,20 @@
+﻿// istanzio una lista di interi
+List<int> numeri = new List<int> { 5, 7, 2, 8, 3 };
+// inizializzo le variabili max e min con il primo elemento della lista
+int max = numeri[0];
+int min = numeri[0];
+// ciclo la lista e confronto ogni elemento con max e min
+foreach (int num in numeri)
+{
+    if (max <= num)
+    {
+        max = num;
+    }
+    else if (min >= num)
+    {
+        min = num;
+    }
+}
+// stampo i risultati
+Console.WriteLine($"Il numero più grande è: {max}");
+Console.WriteLine($"Il numero più piccolo è: {min}");
